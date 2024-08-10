2186768cd0c6582d47c8377ff75b34b24fce2ea7
tthres <qoeuks649@126.com>
2024-08-10 23:49:51 +0800

    [feat]:qss

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QFile file(":/styles/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    a.setStyleSheet(styleSheet);
+
     QTranslator       translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages)
